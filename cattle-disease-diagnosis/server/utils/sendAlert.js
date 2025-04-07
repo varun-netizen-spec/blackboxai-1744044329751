f5c1a9bf4553ef3114bc797b7b4211c18ce8e8fc
varun-netizen-spec <varunvarma867@gmail.com>
2025-04-07 22:16:34 +0530

    Add cattle-disease-diagnosis/server/utils/sendAlert.js 1744044390998

---
+++ cattle-disease-diagnosis/server/utils/sendAlert.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+// Email transporter configuration
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+module.exports = {
+  sendEmailAlert: (to, subject, text) => {
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+  }
+};
